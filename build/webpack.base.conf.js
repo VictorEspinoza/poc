b51d136995393ce5076d02c236a260cda69f0435
victor espinoza <victor.espinoza@enrian.com>
2018-05-24 17:18:43 +0200

    - sass loaders

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -13,7 +13,7 @@ function resolve (dir) {
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: ['./src/main.js']
+    app: ['./src/main.js', './src/sass/main.scss']
   },
   output: {
     path: config.build.assetsRoot,
