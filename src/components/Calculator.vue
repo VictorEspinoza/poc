3f3b97244e348d1bc53d9035b3415fea9a7c8db2
Jan Prchal <prchal.honzak@gmail.com>
2018-05-24 18:00:28 +0200

    styles, structure

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="calculator">
     <h1>Find the Right Buy-to-Let mortgage for you</h1>
-    <p>What Type of a Loan are you looking for? </p>
+    <p class="mb-25">What Type of a Loan are you looking for? </p>
 
-    <div>
-      <label for="value">Property value</label>
-      <input id="value" type="number"/>
-    </div>
-    <div>
-      <vue-slider ref="value" v-model="value"></vue-slider>
+    <div class="calculator__item">
+      <div class="calculator__input">
+        <label for="value">Property value</label>
+        <input id="value" type="number"/>
+      </div>
+
+      <div class="calculator__slider">
+        <vue-slider ref="value" v-model="value"></vue-slider>
+      </div>
     </div>
 
-    <div>
-      <label for="amount"/>Loan Amount
-      <input id="amount" type="number"/>
-    </div>
-    <div>
-      <vue-slider ref="amount" v-model="amount"></vue-slider>
+    <div class="calculator__item">
+      <div class="calculator__input">
+        <label for="amount"/>Loan Amount
+        <input id="amount" type="number"/>
+      </div>
+
+      <div class="calculator__slider">
+        <vue-slider ref="amount" v-model="amount"></vue-slider>
+      </div>
     </div>
   </div>
 </template>
